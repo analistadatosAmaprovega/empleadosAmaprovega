32467cb2301e3abe6e9978f69d968d130481ed51
AnalistaDatos <analistadatosgruporuiz@gmail.com>
2026-07-24 14:36:20 -0400

    Ajustes de Conexion

--- backend/database/db.js
+++ backend/database/db.js
@@ -10,14 +10,14 @@ console.log({
 });
 
 
-// MariaDB
-// const mariadb = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     connectionLimit: 5
-// }).promise();
+MariaDB
+const mariadb = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 5
+}).promise();
 
 // PostgreSQL
 // const postgres = new Pool({

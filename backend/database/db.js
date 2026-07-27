1a508255f3c213d68c52084eb4cb753c4b3edba6
AnalistaDatos <analistadatosgruporuiz@gmail.com>
2026-07-27 14:16:45 -0400

    Error en commit anterior

--- backend/database/db.js
+++ backend/database/db.js
@@ -19,7 +19,11 @@ const mariadb = mysql.createPool({
     connectionLimit: 5
 }).promise();
 
+
+// 
 // PostgreSQL
+//
+// solo si esta en local
 // const postgres = new Pool({
 //     host: process.env.PG_HOST,
 //     port: Number(process.env.PG_PORT),
@@ -30,12 +34,41 @@ const mariadb = mysql.createPool({
 //         ? { rejectUnauthorized: false } 
 //         : false
 // });
-const postgres = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production" 
-        ? { rejectUnauthorized: false } 
-        : false
-});
+
+
+
+// Si esta en produccion
+// const postgres = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: process.env.NODE_ENV === "production" 
+//         ? { rejectUnauthorized: false } 
+//         : false
+// });
+
+
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+
+const postgres = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: isProduction
+        ? { rejectUnauthorized: false }
+        : false,
+    })
+  : new Pool({
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      database: process.env.PG_DATABASE,
+      user: process.env.PG_USER,
+      password: String(process.env.PG_PASSWORD),
+      ssl: isProduction
+        ? { rejectUnauthorized: false }
+        : false,
+    });
+
+
 module.exports = {
     mariadb,
     postgres
